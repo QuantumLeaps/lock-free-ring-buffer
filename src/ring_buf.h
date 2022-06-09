4730b7767ce2d90c62f7221f4f1ac8856be35ebd
MMS <miro@quantum-leaps.com>
2022-06-09 10:52:16 -0400

    3.0.0
    Improved documentation
    Improved the SUTEST Simple Unit Testing framework

--- src/ring_buf.h
+++ src/ring_buf.h
@@ -1,9 +1,4 @@
-/**
-* @file
-* @brief Lock-free ring buffer
-*/
-/*****************************************************************************
-* Last updated on  2022-02-22
+/*============================================================================
 *
 *                    Q u a n t u m  L e a P s
 *                    ------------------------
@@ -11,35 +6,24 @@
 *
 * Copyright (C) 2021 Quantum Leaps, LLC. All rights reserved.
 *
-* This software is licensed under the following open source MIT license:
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
+* SPDX-License-Identifier: MIT
 *
 * Contact information:
 * <www.state-machine.com>
 * <info@state-machine.com>
+============================================================================*/
+/*!
+* @date Last updated on: 2022-06-09
+*
+* @file
+* @brief Lock-free ring buffer
 */
 #ifndef RING_BUF_H
 #define RING_BUF_H
 
-/*
-* ASSUMPTION:
+/*! Ring buffer counter/index
+*
+* @attention
 * The following RingBufCtr type is assumed to be "atomic" in a target CPU,
 * meaning that the CPU needs to write the whole RingBufCtr in one machine
 * instruction. An example of violating this assumption would be an 8-bit
@@ -56,22 +40,40 @@
 */
 typedef uint16_t RingBufCtr;
 
+/*! Ring buffer element type
+*
+* @details
+* The type of the ring buffer elements is not critical for the lock-free
+* operation and does not need to be "atomic". For example, it can be
+* an integer type (uint16_t, uint32_t, uint64_t), a pointer type,
+* or even a struct type. However, the bigger the type the more RAM is
+* needed for the ring buffer and more CPU cycles are needed to copy it
+* into and out of the buffer memory.
+*/
 typedef uint8_t RingBufElement;
 
+/*! Ring buffer struct */
 typedef struct {
     RingBufElement *buf; /*!< pointer to the start of the ring buffer */
     RingBufCtr     end;  /*!< offset of the end of the ring buffer */
     RingBufCtr volatile head; /*!< offset to where next el. will be inserted */
-    RingBufCtr volatile tail; /*!< offset of where next el. will be extracted */
+    RingBufCtr volatile tail; /*!< offset of where next el. will be removed */
 } RingBuf;
-
-typedef void (*RingBufHandler)(RingBufElement const el);
 
 void RingBuf_ctor(RingBuf * const me,
                   RingBufElement sto[], RingBufCtr sto_len);
+RingBufCtr RingBuf_num_free(RingBuf * const me);
 bool RingBuf_put(RingBuf * const me, RingBufElement const el);
 bool RingBuf_get(RingBuf * const me, RingBufElement *pel);
-void RingBuf_process_all(RingBuf * const me, RingBufHandler handler);
-RingBufCtr RingBuf_num_free(RingBuf * const me);
 
-#endif /* RING_BUF */
+/*! Ring buffer callback function for RingBuf_process_all()
+*
+* @details
+* The callback processes one element and runs in the context of
+* RingBuf_process_all().
+*/
+typedef void (*RingBufHandler)(RingBufElement const el);
+
+void RingBuf_process_all(RingBuf * const me, RingBufHandler handler);
+
+#endif /* RING_BUF_H */
